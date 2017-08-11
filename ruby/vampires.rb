c5168d4a303cc8a0c5cebb3d1789530bd9f77d6a
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-11 13:22:35 -0700

    Change datatype of age and year to int

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,9 +3,37 @@ name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
+age = age.to_i
 
 puts "What year were you born?"
 year = gets.chomp
+year = year.to_i
 
 puts "Would you like garlic bread?"
 garlic = gets.chomp
+if garlic = 'y' || 'Y' || 'yes'
+  garlic = true
+else
+  garlic = false
+end
+
+puts "Would you like to enroll in the company health insurance?"
+insurance = gets.chomp
+if insurance = 'y' || 'Y' || 'yes'
+  insurance = true
+else
+  insurace = false
+end
+
+current_year = 2017
+p current_year
+p year
+p age
+
+# def age_right(current_year, year, age)
+#   calc_age = (current_year - year)
+#   if calc_age == age
+#   end
+# end
+
+# age_right(current_year, year, age)
