d47d7684c6f8f026803eb7b3b9150c7779db626d
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-11 14:25:23 -0700

    Add loop for muliple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,54 +1,62 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees today?"
+employees = gets.chomp
+employees = employees.to_i
 
-puts "How old are you?"
-age = gets.chomp
-age = age.to_i
+while employees >= 1
 
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "What year were you born?"
-year = gets.chomp
-year = year.to_i
+  puts "How old are you?"
+  age = gets.chomp
+  age = age.to_i
 
+  puts "What year were you born?"
+  year = gets.chomp
+  year = year.to_i
 
-puts "Would you like garlic bread?"
-garlic = gets.chomp
-if garlic = 'y' || 'Y' || 'yes' || 'Yes'
-  garlic = true
-else
-  garlic = false
-end
-
-puts "Would you like to enroll in the company health insurance?"
-insurance = gets.chomp
-if insurance = 'y' || 'Y' || 'yes' || 'Yes'
-  insurance = true
-else
-  insurance = false
-end
-
-current_year = 2017
-
-def age_right(current_year, year, age)
-  calc_age = (current_year - year)
-  if calc_age == age
-    age_right = true
+  puts "Would you like garlic bread?"
+  garlic = gets.chomp
+  if garlic = 'y' || 'Y' || 'yes' || 'Yes'
+    garlic = true
   else
-    age_right = false
+    garlic = false
   end
+
+  puts "Would you like to enroll in the company health insurance?"
+  insurance = gets.chomp
+  if insurance = 'y' || 'Y' || 'yes' || 'Yes'
+    insurance = true
+  else
+    insurance = false
+  end
+
+  current_year = 2017
+
+  def age_right(current_year, year, age)
+    calc_age = (current_year - year)
+    if calc_age == age
+      age_right = true
+    else
+      age_right = false
+    end
+  end
+
+  age_right(current_year, year, age)
+
+  if name == "Drake Cula"
+    puts "Definitely a vampire."
+  elsif name == "Tu Fang"
+    puts "Definitely a vampire."
+  elsif age_right(current_year, year, age) && garlic && insurance
+    puts "Probably not a vampire."
+  elsif !(age_right(current_year, year, age)) && (garlic || insurance)
+    puts "Probably a vampire."
+  elsif !(age_right(current_year, year, age) &&  garlic && insurance)
+    puts "Almost certainly a vampire."
+  else
+    puts "Results inconclusive"
+  end
+
+  employees -= 1
 end
-
-age_right(current_year, year, age)
-
-if name = "Drake Cula" || "Tu Fang"
-  puts "Definitely a vampire."
-elsif age_right(current_year, year, age) && garlic && insurance
-  puts "Probably not a vampire."
-elsif !(age_right(current_year, year, age)) && (garlic || insurance)
-  puts "Probably a vampire."
-elsif !(age_right(current_year, year, age) &&  garlic && insurance)
-  puts "Almost certainly a vampire."
-else
-  puts "Results inconclusive"
-end
-
