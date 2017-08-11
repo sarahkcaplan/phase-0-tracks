547d446842c607f90123be95a69d97850b366e16
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-11 13:03:32 -0700

    Add age and garlic questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,2 +1,11 @@
 puts "What is your name?"
 name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Would you like garlic bread?"
+garlic = gets.chomp
