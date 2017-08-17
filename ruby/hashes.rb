97d2a54bf6f25f4299b15c0a1d6a0ddf55fba760
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-17 15:53:21 -0700

    Write pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+# PSEUDOCODE
+# -Ask user for information
+#   -Use question as key for hash
+# -Store information in variables
+# -Create a hash with keys and variables
+# -Print out the hash
+# -Ask user if any keys need to be updated
+# -If user input is "none"
+#   -Print hash to the screen
+#   -Break
+# -Else turn input into symbols
+#   -Use interpolation to allow correct key to be updated
+#   -Update hash
+#   -Print the updated hash to the screen
