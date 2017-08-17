26a83390a087649bf2d7c04476771a78ddcf4cb3
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-17 14:25:52 -0700

    Add four answers

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -10,10 +10,10 @@
 # "zom".<???>
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
@@ -22,18 +22,18 @@
 # " suspects".<???>
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".center(27)
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
