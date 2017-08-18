75c1c219609969694bccc3fc56e5b36bd18c889e
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-18 16:16:22 -0700

    Add each for array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,14 +1,40 @@
-# Release 0
+# # Release 0
 
-def hungry
-  puts "Are you hungry?"
-  yield ("eggs")
-end
+# def hungry
+#   puts "Are you hungry?"
+#   yield ("eggs")
+# end
 
-hungry { |food| puts "Same here. I want to eat #{food}"}
+# hungry { |food| puts "Same here. I want to eat #{food}"}
 
 # Relase 1
 
 colors = ['blue', 'green', 'yellow', 'red']
 
 months = {'birthday' => 'October', 'favorite' => 'June', 'worst' => 'February'}
+
+puts "Before .map:"
+p colors
+
+colors.map do |color|
+  color.next
+end
+
+puts "After .map:"
+p colors
+
+puts "Before .map!:"
+p colors
+
+colors.map! do |color|
+  color.next
+end
+
+puts "After .map!:"
+p colors
+
+puts "Array with each:"
+colors.each {|color| puts color}
+
+puts "Hash with each:"
+exi
