d891827ebd34bef252455b50ef9039f2f2cd352e
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-18 12:44:46 -0700

    Add array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,14 @@
+# Release 0
+
 def hungry
   puts "Are you hungry?"
   yield ("eggs")
 end
 
 hungry { |food| puts "Same here. I want to eat #{food}"}
+
+# Relase 1
+
+colors = ['blue', 'green', 'yellow', 'red']
+
+months = {'birthday' => 'October', 'favorite' => 'June', 'worst' => 'February'}
