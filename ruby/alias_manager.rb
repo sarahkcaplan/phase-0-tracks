4a13e7a103e23b9173216431463d6f8a38d594b0
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-18 20:00:50 -0700

    Attempt letter split

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,3 +4,7 @@
 # - List each letter in the names
 # - For each letter in 'aeiou' letter.next
 # - For each letter in 'bcdfghjklmnpqrstvxyz' letter.next
+
+real_name = "Felicia Torres"
+name_array = real_name.split(' ')
+p name_letters = name_array.reverse.split('')
