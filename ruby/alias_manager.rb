8f886a0dc24b53e4693334b4a5f36a42b4cc4c3f
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-18 19:08:19 -0700

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,6 @@
+# PSEUDOCODE
+# -Turn name into an array
+# - Reverse the items in the array
+# - List each letter in the names
+# - For each letter in 'aeiou' letter.next
+# - For each letter in 'bcdfghjklmnpqrstvxyz' letter.next
