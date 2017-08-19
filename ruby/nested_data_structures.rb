32ec1202b5951cf14c7b4267dcb534ab187b210e
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-18 18:47:40 -0700

    Updated data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,18 @@
-travel =
-{"USA"=>
-  ["NYC"= {
-    "attraction" => "Empire State Building",
-    "food"=> "Bao Haus",
-    "transportation" => "subway"},
-  "LA"= {
-    "attraction" => "beach",
-    "food" => "ramen",
-    "transportation" => "car"},
-  "SF",
-  "ABQ"],
-"South Africa"=>
-  ["JNB"]
+travel = {
+  "USA": {
+    "NYC": {
+      "attraction" => "Empire State Building",
+      "food"=> "Bao Haus",
+      "transportation" => "subway"
+      },
+    "LA": {
+      "attraction" => "beach",
+      "food" => "ramen",
+      "transportation" => "car"},
+    "SF",
+    "ABQ"],
+  "South Africa":
+      ["JNB"]
 }
 
 travel[:USA][1][:transportation]
