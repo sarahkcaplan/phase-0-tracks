2290420d537475f2d7c7ca89053c6cad2820b11d
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-20 22:20:31 -0700

    Initial commit

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,10 @@
+# Release 0
+
+def search_array(array, value)
+  new_array = []
+  search_array.each{|value| != value new_array.push(value)}
+  p search_array
+end
+
+array = [2,3,5,6]
+p search_array(array, 5)
