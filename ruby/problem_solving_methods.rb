4ccded8ceccc1d823875281a3a58e9a1590f8cee
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-21 18:58:29 -0700

    Add fib()

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,20 +1,43 @@
-# Release 0
-def search_array(array, value)
+# # Release 0
+# def search_array(array, value)
+#   index = 0
+#   new_array = []
+#   while index < array.length
+#     if array[index] == value
+#       new_array.push(value)
+#       break
+#     else
+#       new_array.push(value)
+#       index += 1
+#     end
+#   end
+#   puts new_array.length - 1
+# end
+
+# array = [2,3,4,6,5,6]
+# value = 5
+# search_array(array, value)
+
+# Release 1
+def fib(x)
+  array = [0,1]
   index = 0
-  new_array = []
-  while index < array.length
-    if array[index] == value
-      new_array.push(value)
-      break
-    else
-      new_array.push(value)
-      index += 1
-    end
+  until array.length == x
+    n1 = index
+    n2 = index + 1
+    n3 = n1 + n2
+    array.push(n3)
+    index += 1
   end
-  puts new_array.length - 1
+  puts array
+  puts array.last
 end
 
-array = [2,3,4,6,5,6]
-value = 5
-search_array(array, value)
+fib(5)
 
+# # PSEUDOCODE
+# array[0] + array[1] = array[2]
+# array[1] + array[2] = array[3]
+# array[2] + array[3] = array[4]
+# until array.length = x
+# print array.last
