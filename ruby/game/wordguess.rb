b9686c29ef5e029e3aafcfcfbc7c53af886de649
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-26 16:53:51 -0700

    Add skeleton of methods to class Game

---
+++ ruby/game/wordguess.rb
@@ -0,0 +1,21 @@
+class Game
+
+  def initialize()
+    puts "Welcome! Let's play."
+  end
+
+  def game_word()
+  end
+
+  def guesses_allowed()
+  end
+
+  def letters_guessed()
+  end
+
+  def game_progress()
+  end
+
+  def guesses_remaining()
+  end
+end
