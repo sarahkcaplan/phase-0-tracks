d5d19673431b8fb1cfd9aa9d0d2adeeb7ac17437
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-26 17:25:09 -0700

    Add test for number of guesses allowed

--- ruby/game/wordguess_spec.rb
+++ ruby/game/wordguess_spec.rb
@@ -6,4 +6,7 @@ describe Game do
   it "stores a word for the game on initialization" do
   expect(game.letters_of_word()).to eq ['b','a','l','d','h','a','w','k']
   end
+
+  it "gives guesses equal to the length of the word plus 3"
+  expect(guesses_allowed(['b','a','l','d','h','a','w','k'])).to eq 11
 end
