6b85f0fe874dc7604351054912516ee8504ef8f9
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-26 17:44:35 -0700

    Add guesses allowed test

--- ruby/game/wordguess_spec.rb
+++ ruby/game/wordguess_spec.rb
@@ -4,9 +4,10 @@ describe Game do
   let(:game) { Game.new("baldhawk") }
 
   it "stores a word for the game on initialization" do
-  expect(game.letters_of_word()).to eq ['b','a','l','d','h','a','w','k']
+    expect(game.letters_of_word()).to eq ['b','a','l','d','h','a','w','k']
   end
 
-  it "gives guesses equal to the length of the word plus 3"
-  expect(guesses_allowed(['b','a','l','d','h','a','w','k'])).to eq 11
+  it "gives guesses equal to the length of the word plus 3" do
+    expect(game.guesses_allowed(['b','a','l','d','h','a','w','k'])).to eq 11
+  end
 end
