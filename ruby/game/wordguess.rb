6609d0e6a16b161b2d456c8bfc69641de9b520e1
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-27 20:47:29 -0700

    Add letters_of_word instance variable to initialize method

--- ruby/game/wordguess.rb
+++ ruby/game/wordguess.rb
@@ -1,10 +1,11 @@
 class Game
 
-attr_accessor :word, :progress
+attr_accessor :word, :progress, :letters_of_word
 
   def initialize(word)
     @word = word
     @progress = []
+    @letters_of_word = []
   end
 
   def game_word()
@@ -50,25 +51,25 @@ end
 # wordguess.find_replace_letters(['h','a','w','k'], 'a')
 
 
-# Driver code
-puts "Welcome! Let's play."
-puts "Enter a word."
-wordguess.word = gets.chomp
-wordguess = Game.new(word)
-letters_of_word = wordguess.game_word()
-guesses_allowed = wordguess.guesses_allowed(letters_of_word)
-count = 0
-while count < guesses_allowed && wordguess.word != wordguess.progress
-  puts "Guess a letter."
-  letter = gets.chomp
-  if wordguess.compare_guess_to_word(letters_of_word, letter) == true
-    puts "Got one!"
-    wordguess.find_replace_letters(letters_of_word, letter)
-    puts wordguess.progress
-    count += 1
-  else
-    puts "Nope."
-    count += 1
-    puts wordguess.progress
-  end
-end
+# # Driver code
+# puts "Welcome! Let's play."
+# puts "Enter a word."
+# word = gets.chomp
+# wordguess = Game.new(word)
+# letters_of_word = wordguess.game_word()
+# guesses_allowed = wordguess.guesses_allowed(letters_of_word)
+# count = 0
+# while count < guesses_allowed && wordguess.word != wordguess.progress
+#   puts "Guess a letter."
+#   letter = gets.chomp
+#   if wordguess.compare_guess_to_word(letters_of_word, letter) == true
+#     puts "Got one!"
+#     wordguess.find_replace_letters(letters_of_word, letter)
+#     puts wordguess.progress
+#     count += 1
+#   else
+#     puts "Nope."
+#     count += 1
+#     puts wordguess.progress
+#   end
+# end
