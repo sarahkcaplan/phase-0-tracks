4eccb145d03cce4cb55e656a9b8e54c2fb1feafb
Sarah Caplan <caplan.sarah@gmail.com>
2017-08-29 10:55:49 -0700

    Add reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,11 +4,13 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# Improting the contents of state_data.rb in this file.
+# Require_relative assumes the file is within the same directory.
 require_relative 'state_data'
 
 class VirusPredictor
+
+# Intitialized an instance of the class and sets instance variables
 
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
@@ -16,32 +18,36 @@ class VirusPredictor
     @population_density = population_density
   end
 
+# A method calling two private methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths()
+    speed_of_spread()
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+# Preforms calculation based on population density to predict the number of deaths and prints the result
+  def predicted_deaths()
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      factor = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      factor = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      factor = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      factor = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      factor = 0.05
     end
+    number_of_deaths = (@population * factor).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# Calculates speed of spread based on population density and prints the results
+  def speed_of_spread() #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,19 +75,40 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |state_name, population_data|
+  us_state = VirusPredictor.new(state_name, population_data[:population_density], population_data[:population])
+  us_state.virus_effects
+end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
-#=======================================================================
-# Reflection Section
+# #=======================================================================
+# # Reflection Section
+
+# # What are the differences between the two different hash syntaxes shown in the state_data file?
+# --One was a hash rocket and the other used symbol colon value
+
+# # What does require_relative do? How is it different from require?
+# -- Both are used for improting the contents of state_data.rb in this file.Require_relative assumes the file is within the same directory. Relative looks outside the working directory.
+
+# # What are some ways to iterate through a hash?
+# --Using #each
+# --A while loop
+
+# # When refactoring virus_effects, what stood out to you about the variables, if anything?
+# --All of the variables were instance variables therefore none need to be passed into the instance method virus_effects
+
+
+# What concept did you most solidify in this challenge?
+# --Using instance variables
