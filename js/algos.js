76de4c793bad7902f91aa84a7788c65c5c79b663
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-03 20:46:28 -0700

    Pseudocode longest phrase

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+// Pseudocode
+Longest phrase
+input: An array of words or phrases
+steps:
+--Loop over the array
+--Get length of each string
+--Save the lengths in a new ordered array
+--Bubble sort the lengths to get the biggest number 'n'at the end
+--Loop over the original array again asking to return the word or phrase where the length = n
+output:Longest word or phrase in the array
+
+Key-Value Match
+input: Two objects
+steps:
+output: boolean
+
+Random Test Data
+input: An integer n
+steps:
+output:An array of length n made up of strings
