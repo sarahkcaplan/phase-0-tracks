611777db06fe5655d75fe80ede5d097fcd273871
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-08 13:55:23 -0700

    Add friends table to db

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -10,4 +10,16 @@ create_quotes_table_cmd = <<-SQL
   )
 SQL
 
+create_friends_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS friends(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    date TEXT,
+    quote INT,
+    success BOOLEAN,
+    FOREIGN KEY(quote) REFERENCES quotes(id)
+  )
+SQL
+
 db.execute(create_quotes_table_cmd)
+db.execute(create_friends_table_cmd)
