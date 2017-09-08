ea8d714e11f0a2fc4f8db9d95b679d1a4437bb53
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-08 13:48:30 -0700

    Create quotes table

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -2,3 +2,12 @@ require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("interestingquotes.db")
+
+create_quotes_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS quotes(
+    id INTEGER PRIMARY KEY,
+    quote VARCHAR(1000)
+  )
+SQL
+
+db.execute(create_quotes_table_cmd)
