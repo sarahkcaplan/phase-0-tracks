6a2de9bfa832f5aec2a1deb6c5310c858bb43b7b
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-08 22:17:04 -0700

    Reorganize into class Wingperson

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -3,76 +3,96 @@ require 'faker'
 
 db = SQLite3::Database.new("interestingquotes.db")
 
-create_quotes_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS quotes(
-    id INTEGER PRIMARY KEY,
-    quote VARCHAR(1000)
-  )
-SQL
+def wingperson
 
-create_successes_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS successes(
-    id INTEGER PRIMARY KEY,
-    friend INT,
-    quote INT,
-    success BOOLEAN,
-    FOREIGN KEY(friend) REFERENCES friends(id),
-    FOREIGN KEY(quote) REFERENCES quotes(id)
-  )
-SQL
+  def initialize
+    @quote_id = ''
+    @friend_name = ''
+    @friend_id = ''
+    @success = ''
+  end
 
-create_friends_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS friends(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    gender VARCHAR(10)
-  )
-SQL
+  def add_quotes(db, quote) # needed to get interesting quotes about your friend
+    db.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
+  end
 
-def add_friends(db, name, gender)
-  db.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
+  def add_friends(db, name, gender) # for dummy data only
+    new_friend = db.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
+    @friend_id = new_friend
+  end
+
+  def add_past_successes(db, friend, quote, success) # for dummy data only
+    db.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
+  end
+
+  def get_new_quote
+    number = rand(200)
+    db.execute("SELECT quote FROM quotes WHERE id = #{number} ")
+  end
+
+  def rate_success(rating)
+    if reaction == "Great!"
+      @success = true
+    else reaction == "Uhhm..."
+      @success = false
+    end
+    db.execute(create_success_rating)
+  end
+
+  create_success_rating = <<-SQL
+    INSERT INTO successes(
+      friend, quote, success)
+    VALUES( @friend_id, @quote_id, @success)
+  SQL
+
+  create_quotes_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS quotes(
+      id INTEGER PRIMARY KEY,
+      quote VARCHAR(1000)
+    )
+  SQL
+
+  create_friends_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS friends(
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(255),
+      gender VARCHAR(10)
+    )
+  SQL
+
+  create_successes_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS successes(
+      id INTEGER PRIMARY KEY,
+      friend INT,
+      quote INT,
+      success BOOLEAN,
+      FOREIGN KEY(friend) REFERENCES friends(id),
+      FOREIGN KEY(quote) REFERENCES quotes(id)
+    )
+  SQL
 end
 
-def add_quotes(db, quote)
-  db.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
-end
+## Use to create test data
 
-def add_past_successes(db, friend, quote, success)
-  db.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
-end
-
-10.times do
-  add_past_successes(db, rand(1..10), rand(1..200), rand(2))
-end
-
-# 200.times do
-#   add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
+# 10.times do
+#   add_past_successes(db, rand(1..10), rand(1..200), rand(2))
 # end
 
 # 10.times do
 #   add_friends(db, Faker::Name.name, Faker::Demographic.sex)
 # end
 
-
+## Use to remove test data tables
 # db.execute("DROP TABLE friends")
 # db.execute("DROP TABLE quotes")
 # db.execute("DROP TABLE successes")
 
-
-
+## Driver code
 db.execute(create_quotes_table_cmd)
 db.execute(create_friends_table_cmd)
 db.execute(create_successes_table_cmd)
 
-# friend_full_name = db.execute("SELECT name FROM friends;")
-
-# def get_first_name(friend_full_name)
-#   friend_full_name.each do |name|
-#     friend_full_name.split(" ")
-#     puts friend_full_name
-#     friend_first_name = friend_full_name[0]
-#     puts friend_first_name
-#   end
+# 200.times do
+#   add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
 # end
 
-# get_first_name(friend_full_name)
