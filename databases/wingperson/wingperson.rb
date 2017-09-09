ec6e1fbd8650cc794c88bb3b9190ac78eeae2686
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-08 18:54:03 -0700

    Add gender to friend data

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -15,27 +15,28 @@ create_friends_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS friends(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
+    gender VARCHAR(10),
     quote INT,
     success BOOLEAN,
     FOREIGN KEY(quote) REFERENCES quotes(id)
   )
 SQL
 
-def add_friends(db, name, quote, success)
-  db.execute("INSERT INTO friends (name, quote, success) VALUES (?, ?, ?)", [name, quote, success])
+def add_friends(db, name, gender, quote, success)
+  db.execute("INSERT INTO friends (name, gender, quote, success) VALUES (?, ?, ?, ?)", [name, gender, quote, success])
 end
 
-def add_quotes(db, quote)
-  db.execute("INSERT INTO QUOTES (quote) VALUES (?)", [quote])
-end
-
-200.times do
-  add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
-end
-
-# 10.times do
-#   add_friends(db, Faker::Name.name, 0, 'false')
+# def add_quotes(db, quote)
+#   db.execute("INSERT INTO QUOTES (quote) VALUES (?)", [quote])
 # end
+
+# 200.times do
+#   add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
+# end
+
+10.times do
+  add_friends(db, Faker::Name.name, Faker::Demographic.sex, 0, 'false')
+end
 
 db.execute(create_quotes_table_cmd)
 # db.execute("DROP TABLE friends")
