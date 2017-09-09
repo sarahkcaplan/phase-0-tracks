ea13beca75e6df16ea9a72a7ab23124480377dd6
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-08 23:00:43 -0700

    Add driver code with user prompts

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -1,9 +1,9 @@
 require 'sqlite3'
 require 'faker'
 
-db = SQLite3::Database.new("interestingquotes.db")
+DB = SQLite3::Database.new("interestingquotes.DB")
 
-def wingperson
+class Wingperson
 
   def initialize
     @quote_id = ''
@@ -13,30 +13,32 @@ def wingperson
   end
 
   def add_quotes(db, quote) # needed to get interesting quotes about your friend
-    db.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
+    DB.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
   end
 
   def add_friends(db, name, gender) # for dummy data only
-    new_friend = db.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
+    new_friend = DB.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
     @friend_id = new_friend
   end
 
   def add_past_successes(db, friend, quote, success) # for dummy data only
-    db.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
+    DB.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
   end
 
   def get_new_quote
     number = rand(200)
-    db.execute("SELECT quote FROM quotes WHERE id = #{number} ")
+    DB.execute("SELECT quote FROM quotes WHERE id = #{number} ")
   end
 
-  def rate_success(rating)
+  def rate_success(reaction)
     if reaction == "Great!"
       @success = true
-    else reaction == "Uhhm..."
+    elsif reaction == "Uhhm..."
+      @success = false
+    else
       @success = false
     end
-    db.execute(create_success_rating)
+    DB.execute(create_success_rating)
   end
 
   create_success_rating = <<-SQL
@@ -44,55 +46,69 @@ def wingperson
       friend, quote, success)
     VALUES( @friend_id, @quote_id, @success)
   SQL
-
-  create_quotes_table_cmd = <<-SQL
-    CREATE TABLE IF NOT EXISTS quotes(
-      id INTEGER PRIMARY KEY,
-      quote VARCHAR(1000)
-    )
-  SQL
-
-  create_friends_table_cmd = <<-SQL
-    CREATE TABLE IF NOT EXISTS friends(
-      id INTEGER PRIMARY KEY,
-      name VARCHAR(255),
-      gender VARCHAR(10)
-    )
-  SQL
-
-  create_successes_table_cmd = <<-SQL
-    CREATE TABLE IF NOT EXISTS successes(
-      id INTEGER PRIMARY KEY,
-      friend INT,
-      quote INT,
-      success BOOLEAN,
-      FOREIGN KEY(friend) REFERENCES friends(id),
-      FOREIGN KEY(quote) REFERENCES quotes(id)
-    )
-  SQL
 end
+
+create_quotes_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS quotes(
+    id INTEGER PRIMARY KEY,
+    quote VARCHAR(1000)
+  )
+SQL
+
+create_friends_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS friends(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    gender VARCHAR(10)
+  )
+SQL
+
+create_successes_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS successes(
+    id INTEGER PRIMARY KEY,
+    friend INT,
+    quote INT,
+    success BOOLEAN,
+    FOREIGN KEY(friend) REFERENCES friends(id),
+    FOREIGN KEY(quote) REFERENCES quotes(id)
+  )
+SQL
 
 ## Use to create test data
 
 # 10.times do
-#   add_past_successes(db, rand(1..10), rand(1..200), rand(2))
+#   add_past_successes(DB, rand(1..10), rand(1..200), rand(2))
 # end
 
 # 10.times do
-#   add_friends(db, Faker::Name.name, Faker::Demographic.sex)
+#   add_friends(DB, Faker::Name.name, Faker::Demographic.sex)
 # end
 
 ## Use to remove test data tables
-# db.execute("DROP TABLE friends")
-# db.execute("DROP TABLE quotes")
-# db.execute("DROP TABLE successes")
+# DB.execute("DROP TABLE friends")
+# DB.execute("DROP TABLE quotes")
+# DB.execute("DROP TABLE successes")
 
 ## Driver code
-db.execute(create_quotes_table_cmd)
-db.execute(create_friends_table_cmd)
-db.execute(create_successes_table_cmd)
+# DB.execute(create_quotes_table_cmd)
+# DB.execute(create_friends_table_cmd)
+# DB.execute(create_successes_table_cmd)
+
+w1 = Wingperson.new
 
 # 200.times do
-#   add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
+#   w1.add_quotes(DB, Faker::MostInterestingManInTheWorld.quote)
 # end
+
+puts "You're an awesome wingperson. What's your friend's name?"
+friend_name = gets.chomp
+w1.add_friends(DB, friend_name, 'male')
+puts "Drop this 'fact' about #{friend_name} into conversation. #{friend_name} is sooo interesting!"
+puts
+puts w1.get_new_quote
+puts
+puts "So, how did that go? (Great! or Uhmm...)"
+reaction = gets.chomp
+w1.rate_success(reaction)
+puts "Well, you're a great wingperson."
 
