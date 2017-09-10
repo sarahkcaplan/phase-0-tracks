2237e5f8fb4e2f68809d6f33b0027fee9969de7e
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-09 21:20:13 -0700

    Commit before switching branches

--- databases/wingperson/wingperson.rb
+++ databases/wingperson/wingperson.rb
@@ -1,51 +1,46 @@
 require 'sqlite3'
 require 'faker'
 
-DB = SQLite3::Database.new("interestingquotes.DB")
+db = SQLite3::Database.new("interestingquotes.db")
 
-class Wingperson
+def add_quotes(db, quote) # needed to get interesting quotes about your friend
+  db.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
+end
 
-  def initialize
-    @quote_id = ''
-    @friend_name = ''
-    @friend_id = ''
-    @success = ''
-  end
+def add_friends(db, name, gender) # for dummy data only
+  new_friend = db.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
+  @friend_id = new_friend
+end
 
-  def add_quotes(db, quote) # needed to get interesting quotes about your friend
-    DB.execute("INSERT INTO quotes (quote) VALUES (?)", [quote])
-  end
+def add_past_successes(db, friend, quote, success) # for dummy data only
+  db.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
+end
 
-  def add_friends(db, name, gender) # for dummy data only
-    new_friend = DB.execute("INSERT INTO friends (name, gender) VALUES (?, ?)", [name, gender])
-    @friend_id = new_friend
-  end
+def get_new_quote(db)
+  number = rand(200)
+  db.execute("SELECT quote FROM quotes WHERE id = #{number}")
+end
 
-  def add_past_successes(db, friend, quote, success) # for dummy data only
-    DB.execute("INSERT INTO successes (friend, quote, success) VALUES (?, ?, ?)", [friend, quote, success])
-  end
-
-  def get_new_quote
-    number = rand(200)
-    DB.execute("SELECT quote FROM quotes WHERE id = #{number} ")
-  end
-
-  def rate_success(reaction)
-    if reaction == "Great!"
-      @success = true
-    elsif reaction == "Uhhm..."
-      @success = false
-    else
-      @success = false
-    end
-    DB.execute(create_success_rating)
-  end
-
-  create_success_rating = <<-SQL
+def rate_success(db, reaction, friend_name)
+  if reaction == "Great!"
+    @success = true
+    db.execute("SQL
     INSERT INTO successes(
       friend, quote, success)
-    VALUES( @friend_id, @quote_id, @success)
-  SQL
+    VALUES(#{friend_id}, #{success})")
+  elsif reaction == "Uhhm..."
+    @success = false
+    db.execute("SQL
+    INSERT INTO successes(
+      friend, quote, success)
+    VALUES(#{friend_id}, #{success})")
+  else
+    @success = false
+    db.execute("SQL
+    INSERT INTO successes(
+      friend, quote, success)
+    VALUES(#{friend_id}, #{success})")
+  end
 end
 
 create_quotes_table_cmd = <<-SQL
@@ -74,41 +69,38 @@ create_successes_table_cmd = <<-SQL
   )
 SQL
 
+## Use to remove test data tables
+
+# db.execute("DROP TABLE friends")
+# db.execute("DROP TABLE quotes")
+# db.execute("DROP TABLE successes")
+
 ## Use to create test data
 
+# db.execute(create_quotes_table_cmd)
+# db.execute(create_friends_table_cmd)
+# db.execute(create_successes_table_cmd)
+
 # 10.times do
-#   add_past_successes(DB, rand(1..10), rand(1..200), rand(2))
+#   add_past_successes(db, rand(1..10), rand(1..200), rand(2))
 # end
 
 # 10.times do
-#   add_friends(DB, Faker::Name.name, Faker::Demographic.sex)
+#   add_friends(db, Faker::Name.name, Faker::Demographic.sex)
 # end
-
-## Use to remove test data tables
-# DB.execute("DROP TABLE friends")
-# DB.execute("DROP TABLE quotes")
-# DB.execute("DROP TABLE successes")
-
-## Driver code
-# DB.execute(create_quotes_table_cmd)
-# DB.execute(create_friends_table_cmd)
-# DB.execute(create_successes_table_cmd)
-
-w1 = Wingperson.new
 
 # 200.times do
-#   w1.add_quotes(DB, Faker::MostInterestingManInTheWorld.quote)
+#   w1.add_quotes(db, Faker::MostInterestingManInTheWorld.quote)
 # end
 
+## Driver code
 puts "You're an awesome wingperson. What's your friend's name?"
 friend_name = gets.chomp
-w1.add_friends(DB, friend_name, 'male')
+add_friends(db, friend_name, 'male')
 puts "Drop this 'fact' about #{friend_name} into conversation. #{friend_name} is sooo interesting!"
-puts
-puts w1.get_new_quote
-puts
+puts get_new_quote(db)
 puts "So, how did that go? (Great! or Uhmm...)"
 reaction = gets.chomp
-w1.rate_success(reaction)
+rate_success(db, reaction, friend_name)
 puts "Well, you're a great wingperson."
 
