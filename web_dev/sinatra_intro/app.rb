bfff1287623f2074a53af1c7a3674357cce8d7e3
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-18 07:08:38 -0700

    Add contact address

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,7 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "123 Internet Ln, Hollywood, CA"
+end
